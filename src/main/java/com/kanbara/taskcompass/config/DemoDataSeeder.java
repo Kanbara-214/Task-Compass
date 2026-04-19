a270c99d9a45f978285e77d9651ff5d332836cce
Kanbara-214 <0912mm1b63@gmail.com>
2026-04-20 03:45:55 +0900

    refactor: テスト時に初期データが作成されないよう修正

--- src/main/java/com/kanbara/taskcompass/config/DemoDataSeeder.java
+++ src/main/java/com/kanbara/taskcompass/config/DemoDataSeeder.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ import com.kanbara.taskcompass.mapper.AppUserMapper;
 import com.kanbara.taskcompass.mapper.TaskItemMapper;
 
 @Component
+@Profile("!test")
 public class DemoDataSeeder implements CommandLineRunner {
 
     private final AppUserMapper appUserMapper;
